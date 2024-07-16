3ed974ce628fa0af871c0bb9712baf6a22779fde
Harsh patel <hp98795@gmail.com>
2024-07-16 12:48:12 +0530

    Created window and game-loop

--- Space-Invaders/main.cpp
+++ Space-Invaders/main.cpp
@@ -1,5 +1,26 @@
+#include <SFML/Graphics.hpp>
 
 int main()
 {
+
+    sf::VideoMode videoMode = sf::VideoMode(800, 600);
+    sf::RenderWindow window(videoMode, "Space Invaders");
+    window.setPosition(sf::Vector2i(100, 100));
+
+    while(window.isOpen())
+    {
+        sf::Event gameEvent;
+
+        while(window.pollEvent(gameEvent))
+        {
+            if(gameEvent.type == sf::Event::Closed)
+                window.close();
+        }
+
+        window.clear(sf::Color::Blue);
+
+        window.display();
+    }
+    
     return 0;
 }

48dc280600fd4f9e225d37863e56a741b82344d3
Harsh patel <hp98795@gmail.com>
2024-07-18 11:40:44 +0530

    Created player class
    - Added variables
    - Added functions

--- Space-Invaders/main.cpp
+++ Space-Invaders/main.cpp
@@ -1,43 +1,33 @@
 #include <SFML/Graphics.hpp>
 
+class Player
+{
+public:
+    int getScore() {return _score;}
+    void setScore(int newScore) {_score = newScore;}
+    void takeDamage() {}
+    void move() {}
+    void shootBullets() {}
+    
+    
+public:
+    sf::Texture texture;
+    sf::Sprite sprite;
+
+private:
+    int _score = 0;
+    int _health = 3;
+    int _movementSpeed = 5;
+    sf::Vector2f _position = sf::Vector2f(200.f, 100.f);
+    
+};
+
 int main()
 {
-
     sf::VideoMode videoMode = sf::VideoMode(800, 600);
     sf::RenderWindow window(videoMode, "Space Invaders");
-    window.setPosition(sf::Vector2i(100, 100));
 
-    // Draw a circle
-    sf::CircleShape circle(50); // Radius 50
-    circle.setFillColor(sf::Color::Green);
-    circle.setPosition(400, 300); // Set position
-
-    // Draw a square
-    sf::RectangleShape square(sf::Vector2f(20, 20)); // Size 20x20
-    square.setFillColor(sf::Color::Red);
-    square.setPosition(550, 400); // Set position
-
-    // Draw a triangle
-    sf::ConvexShape triangle(3);
-    triangle.setPoint(0, sf::Vector2f(200, 100));
-    triangle.setPoint(1, sf::Vector2f(300, 300));
-    triangle.setPoint(2, sf::Vector2f(100, 300));
-    triangle.setFillColor(sf::Color::Blue);
-
-    // Sprite
-    sf::Texture outscal_texture;
-    outscal_texture.loadFromFile("assets/textures/outscal_logo.png");
-    sf::Sprite outscal_sprite;
-    outscal_sprite.setTexture(outscal_texture);
-    outscal_sprite.setPosition(200, 100); // Position
-    outscal_sprite.setRotation(45); // Rotation in degrees
-    outscal_sprite.setScale(0.5, 0.5); // Scale factor
-
-    // Text
-    sf::Font font;
-    font.loadFromFile("assets/fonts/DS_DIGIB.ttf");
-    sf::Text text("Hello SFML!", font, 50);
-    text.setFillColor(sf::Color::Red);
+    Player player;
     
     while(window.isOpen())
     {
@@ -50,13 +40,6 @@ int main()
         }
 
         window.clear(sf::Color(50,50,50));
-
-        window.draw(circle);
-        window.draw(square);
-        window.draw(triangle);
-        window.draw(outscal_sprite);
-        window.draw(text);
-
         
         window.display();
     }
